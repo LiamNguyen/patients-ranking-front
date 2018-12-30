c6269189969fb1e71fd293c54213903d4f730169
Quang Minh Nguyen <minhnguyen181199@gmail.com>
2018-12-30 18:09:18 +0200

    prettier fix

--- src/components/screens/PatientsRanking/Header.js
+++ src/components/screens/PatientsRanking/Header.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import moment from 'moment';
 
 import './style.css';
@@ -13,28 +13,28 @@ const {
 class Header extends Component {
   constructor(props) {
     super(props);
-      this.state = {
-        time: new moment().format('HH:mm')
-      };
+    this.state = {
+      time: new moment().format('HH:mm')
+    };
   }
   componentDidMount() {
     setInterval(() => {
       this.setState({
         time: new moment().format('HH:mm')
-      })
-    }, 1000)
+      });
+    }, 1000);
   }
 
   render() {
     return (
-      <div className = "header-container" >
-        <div className = "header-title" >
-        <img className = "nancy-logo"src = {NancyLogo} alt = "" />
-        <span > {headerTitle} </span> 
-        <img className = "htgsoft-logo"src = {HTGSoftLogo} alt = "" />
-        </div> 
-        <div className = "current-time" > {this.state.time} </div> 
+      <div className="header-container">
+        <div className="header-title">
+          <img className="nancy-logo" src={NancyLogo} alt="" />
+          <span> {headerTitle} </span>
+          <img className="htgsoft-logo" src={HTGSoftLogo} alt="" />
         </div>
+        <div className="current-time"> {this.state.time} </div>
+      </div>
     );
   }
 }
